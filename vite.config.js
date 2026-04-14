700bd14309bafb0b01ebdd89ad4c2c50057a2de2
riefkiariyanto <riefkiari199@gmail.com>
2026-04-14 08:15:03 +0700

    update ui login page

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,12 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
         strictPort: true,
+
         hmr: {
             host: "localhost",
+        },
+        watch: {
+            usePolling: true,
         },
     },
     plugins: [
